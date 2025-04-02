9a23fbe8256ce0436b177917ed0866fbaf4e82a6
Amedon Roland <rolandamedon@gmail.com>
2025-04-02 02:17:41 +0000

    Add component documentation and improve styling for button and card components

--- resources/views/components/installation-card.blade.php
+++ resources/views/components/installation-card.blade.php
@@ -1,4 +1,16 @@
 {{-- filepath: resources/views/components/installation-card.blade.php --}}
+{{--
+    Composant : Installation Card
+    Description : Affiche une carte contenant une icône et un titre pour une installation.
+    Props :
+        - $icon (string) : URL de l'icône à afficher.
+        - $title (string) : Texte du titre affiché sous l'icône.
+    Exemple d'utilisation :
+        <x-installation-card
+            icon="{{ asset('icons/pool.svg') }}"
+            title="Piscine"
+        />
+--}}
 <div class="w-[280px] h-[250px] bg-background shadow-md flex flex-col items-center rounded-[5px] justify-center hover:shadow-lg transition-shadow duration-300">
     {{-- Icon --}}
     <img src="{{ $icon }}" alt="{{ $title }}" class="w-13 h-[40px] mb-2">
