b9a6dff07526afabb08f6994b658b4c793865b48
Jean-Paul <adoglijeanpaul@gmail.com>
2025-04-06 19:16:27 +0000

    Ajout du composant CarouselTestimoniesCard avec intégration dans la vue acceuil et mise à jour du style des témoignages.

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -4,15 +4,47 @@
 
    {{-- Profile Card --}}
   
-{{-- Exemple d'utilisation dans une vue --}}
-<x-testimonies-card 
-    date="2 Mar. 2023" 
-    stars="3" 
-    text="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." 
-    image="images/im.jpg" 
-    author="Anthony Bruff" 
-/>
-
+    <x-carousel-testimonies-card :testimonies="[
+    ['date' => '2 Mars 2023', 
+    'stars' => 4, 
+    'text' => 'Le service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit co service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+L   orem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam....', 'image' => '/images/im.jpg', 'author' => 'Anthony Bruff'],
+   
+    ['date' => '25 Mars 2025', 
+    'stars' => 5, 
+    'text' => 'Le service à service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit co l\'hôtel éLorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam.tait exceptionnel...', 'image' => '/images/im.jpg', 'author' => 'Regina Gella'],
+  
+    ['date' => '5 Avril 2025', 'stars' => 3, 'text' => 'Le service service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit co à l\'hôtel étaLorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam.it exceptionnel...', 'image' => '/images/im.jpg', 'author' => 'Jamiyu Aliyu'],
+    
+    ['date' => '2 Mars 2023', 
+    'stars' => 4, 'text' => 'Le service à l\'hôtel Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam es service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit cot sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam.était exceptionnel...', 'image' => '/images/im.jpg', 'author' => 'Anthony Bruff'],
+   
+    ['date' => '25 Mars 2025', 
+    'stars' => 5, 
+    'text' => 'Le service à l\'hôteLorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam es service à l\'hôtel était exceptionnel
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit cot sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam.l était exceptionnel...', 'image' => '/images/im.jpg', 'author' => 'Regina Gella'],
+    
+    ['date' => '5 Avril 2025', 
+    'stars' => 3, 
+    'text' => 'Le service à l\'hôtel était excLorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam  service à l\'hôtel était exceptionnel
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus non eum laboriosam fugit natus incidunt eaque ab suscipit nesciunt cupiditate, commodi officiis explicabo tempora fuga vero doloribus sed ut distinctio.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam est sit coest sit consequatur tempore itaque a, dolores consectetur at molestiae expedita beatae delectus quibusdam ipsum, facere voluptatum saepe iusto velit magnam.eptionnel...', 'image' => '/images/im.jpg', 'author' => 'Jamiyu Aliyu']
+    ]" />  
 
 </div>
 @endsection
